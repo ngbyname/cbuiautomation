c7c3bdbf187507177066eeba9b88d7585e549b53
ashwanirao <raoashwani786@gmail.com>
2023-06-06 17:08:33 +0530

    Driver file changes

--- src/main/java/com/code/bull/driver/Driver.java
+++ src/main/java/com/code/bull/driver/Driver.java
@@ -1,4 +1,16 @@
 package com.code.bull.driver;
 
 public class Driver {
+    public static String evnName = System.getProperty("Env").toUpperCase();
+
+
+    public static void envLevelSetup(){
+        if (evnName.equalsIgnoreCase("sit")) {
+            evnName = "SIT";
+        } else if (evnName.equalsIgnoreCase("uat")) {
+            evnName = "UAT";
+        } else {
+            evnName = "PROD";
+        }
+    }
 }
